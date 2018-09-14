e113381d0759aa7adfd1b5c3a3a3582c1803a129
Rudy Ravelin <rudy@noukod.com>
2018-09-14 14:13:15 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,6 +23,7 @@ end
 
 def valid_phone_number?(phone)
  phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]
- {3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
+ {3}-[0-9]{4})|[0-9]{7})\b)/)?
+ true : false
 
  end
