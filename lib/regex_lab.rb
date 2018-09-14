800f334f893c176be8924bb0960d9b8e447b5929
Rudy Ravelin <rudy@noukod.com>
2018-09-14 14:07:42 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,16 +1,23 @@
 def starts_with_a_vowel?(word)
-
-end
+  if word.match (/^[aeiouAEIOU]/)
+    true
+  else
+    false
+    end
+  end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/un\w+ing/i)
 
 end
 
 def words_five_letters_long(text)
+text.scan(/\b\w{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+ text.match(/^[A-Z].+[\.!?]$/) ? true : false
 
 end
 
